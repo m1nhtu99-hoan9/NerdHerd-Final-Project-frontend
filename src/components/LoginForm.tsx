44cbfdbb3de4e2b966be541d706895b2dd6a77ca
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2020-08-04 00:21:21 +0700

    ✗ mock-up Login Form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,13 +1,18 @@
+import i18n from '../i18n'
 import React from 'react'
-import { Content, Input, Item } from 'native-base'
+import { Content, Input, Item, Text } from 'native-base'
 import * as f from 'formik'
 
-export default function LoginForm (props: unknown) {
+export default function LoginForm(props: unknown) {
   return (
     <Content>
       <Item rounded>
-        <Input placeholder='Ten Dang Nhap'/>
+        <Input placeholder={i18n.t('signIn.usernameInput')} />
       </Item>
+      <Item rounded>
+        <Input placeholder={i18n.t('signIn.passwordInput')} />
+      </Item>
+      <Text>{i18n.t('signIn.forgetPassword')}</Text>
     </Content>
   )
 }
--- src/locale/vn.ts
+++ src/locale/vn.ts
@@ -1,58 +1,56 @@
 const vn = {
-  appName: 'CreScorex', 
-  screens: {
-    signIn: {
-      _nav: 'Sign In',
-      usernameInput: 'Username',
-      passwordInput: 'Password', 
-      forgetPassword: 'Forget Password?',
-      submitBtn: 'Sign In'
-    }, 
-    home: {
-      _nav: 'Home',
-      header: 'Search History', 
-      firstSubHeader: 'Credit Score', 
-      suggestion: 'Suggestion', 
-      suggestionContent: {
-        low: '', 
-        middle: '', 
-        high: ''
-      }, 
-      secondSubHeader: 'Loan Options',
-      result: 'Result:', 
-      resultContent: 'Xác suất vay thành công là %{percentage} phần trăm',
-      consultation: 'Suggestive advices to the customer:',
-      consultationContent: [
-        'Quý khách có thể cân nhắc khoản vay với số tiền ít hơn', 
-        'Quý khách cần duy trì số dư tài khoản hàng tháng tốt hơn để cải thiện điểm tín dụng cá nhân'
-      ], 
-      loanOptionsInput: 'Pick your loan type', 
-      loanAmountInput: 'Your loan amount', 
-      submitBtn: 'Process >' 
-    }, 
-    search: {
-      _nav: 'Search',
-      phoneNumInput: 'Phone Number', 
-      submitBtn: 'Process >'
-    }, 
-    changePassword: {
-      _nav: 'Change Password',
-      oldPassInput: 'Current password', 
-      newPassInput: 'New password', 
-      confirmPassInput: 'Re-type new password',
-      submitBtn: 'Submit'
-    }, 
-    aboutMe: {
-      _nav: 'About Me', 
-      bank: 'Bank', 
-      accId: 'Account ID', 
-      accStatus: 'Account Status', 
-      email: 'Email',
-      phoneNum: 'Phone Number', 
-      signOutBtn: 'Sign Out', 
-      changePassBtn: 'Change Password'
-    }
-  }
+  appName: 'CreScorex',
+  signIn: {
+    _nav: 'Sign In',
+    usernameInput: 'Username',
+    passwordInput: 'Password',
+    forgetPassword: 'Forget Password?',
+    submitBtn: 'Sign In',
+  },
+  home: {
+    _nav: 'Home',
+    header: 'Search History',
+    firstSubHeader: 'Credit Score',
+    suggestion: 'Suggestion',
+    suggestionContent: {
+      low: '',
+      middle: '',
+      high: '',
+    },
+    secondSubHeader: 'Loan Options',
+    result: 'Result:',
+    resultContent: 'Xác suất vay thành công là %{percentage} phần trăm',
+    consultation: 'Suggestive advices to the customer:',
+    consultationContent: [
+      'Quý khách có thể cân nhắc khoản vay với số tiền ít hơn',
+      'Quý khách cần duy trì số dư tài khoản hàng tháng tốt hơn để cải thiện điểm tín dụng cá nhân',
+    ],
+    loanOptionsInput: 'Pick your loan type',
+    loanAmountInput: 'Your loan amount',
+    submitBtn: 'Process >',
+  },
+  search: {
+    _nav: 'Search',
+    phoneNumInput: 'Phone Number',
+    submitBtn: 'Process >',
+  },
+  changePassword: {
+    _nav: 'Change Password',
+    oldPassInput: 'Current password',
+    newPassInput: 'New password',
+    confirmPassInput: 'Re-type new password',
+    submitBtn: 'Submit',
+  },
+  aboutMe: {
+    _nav: 'About Me',
+    bank: 'Bank',
+    accId: 'Account ID',
+    accStatus: 'Account Status',
+    email: 'Email',
+    phoneNum: 'Phone Number',
+    signOutBtn: 'Sign Out',
+    changePassBtn: 'Change Password',
+  },
 }
 
 export default vn
