7abd831c5c0ebf4ec12c8a6cc6fedee8af8200e9
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2020-08-05 16:02:18 +0700

    ✗ minor fix on navigation [✗]

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,18 +1,17 @@
-import { type } from "ramda";
-import { createStackNavigator } from '@react-navigation/stack';
+import { type } from 'ramda'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-export const Stack = createStackNavigator<StackParamList>();
-export const Tab = createBottomTabNavigator<BottomTabParamList>();
+export const Stack = createStackNavigator<StackParamList>()
+export const Tab = createBottomTabNavigator<BottomTabParamList>()
 
-type StackParamList = {
-    Login: {name : number}
-    Home: {userId: string};
-    ChangePassword: {userId: string}
+export type StackParamList = {
+  Login: { name: number }
+  Home: { userId: string }
+  ChangePassword: { userId: string }
 }
 
-type BottomTabParamList = {
-    Index: {userId : string}
-    Search: undefined
-    Profile: {userId : string}
+export type BottomTabParamList = {
+  Index: { userId: string }
+  Search: undefined
+  Profile: { userId: string }
 }
--- src/navigation/stackNavigator.tsx
+++ src/navigation/stackNavigator.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import 'react-native-gesture-handler'
+// import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
-import { Stack } from '../../src/@types/navigation'
+import { createStackNavigator } from '@react-navigation/stack'
+import { StackParamList } from '../../src/@types/navigation'
 import BottomTabNavigator from './bottomTabNavigator'
 
 //Import Screens
-import {LoginScreen} from '../screens/index'
-import ChangePasswordScreen from '../screens/ChangePasswordScreen'
+import { LoginScreen, ChangePasswordScreen } from '../screens/index'
+
+const Stack = createStackNavigator<StackParamList>()
 
 const StackNavigator = () => {
   return (
     <View>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Login">{() => <LoginScreen />}</Stack.Screen>
           <Stack.Screen
             name="Home"
             component={BottomTabNavigator}
