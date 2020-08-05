9295bb39208fb8c573ab332873271c87836df895
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2020-08-05 21:52:29 +0700

    mock up first launch animation effect [✔]

--- App.tsx
+++ App.tsx
@@ -2,16 +2,11 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-//import HomeScreen from './src/screens/HomeScreen'
-import { LoginScreen } from './src/screens/index'
-import { BottomTabNavigator } from './src/navigation/index'
+import AnimatedLoginScreen from './src/components/animated/firstLauch'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>OK</Text>
-    </View>
+    <AnimatedLoginScreen/>
   );
 }
 
