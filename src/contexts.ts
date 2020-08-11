748eeb67fbe608e178e7c5c822ae1e7c76a91934
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2020-08-12 01:27:23 +0700

    minor type enhancements on `context.ts`

--- src/contexts.ts
+++ src/contexts.ts
@@ -3,10 +3,6 @@ import { ParamListBase } from '@react-navigation/routers'
 import { NavigationProp } from '@react-navigation/native'
 
 /**@todo initiates SignInNavContext with a pseudo Screen's navigation object */
-const SignInNavContext = createContext({
-  navigate: (...args: any[]) => {
-    return
-  },
-} as NavigationProp<ParamListBase>)
+const SignInNavContext = createContext({} as NavigationProp<ParamListBase>)
 
 export { SignInNavContext }
