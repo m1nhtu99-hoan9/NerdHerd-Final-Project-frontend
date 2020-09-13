fb17f9fe69c01016b398d699b4bafa4f57ad2c11
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2020-09-14 00:47:49 +0700

    ✔ define api promises

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -2,29 +2,52 @@
   Transient type definitions go here
 */
 
-/** a hack declaring empty type module to avoid type warning regarding 
+/** a hack declaring empty type module to avoid type warning regarding
  * `sync-storage`, which actually use Flow instead of TypeScript
- */ 
+ */
 declare module 'sync-storage'
 
-/** a hack declaring empty type module to avoid type warning 
+/** a hack declaring empty type module to avoid type warning
  *  regarding `rn-faded-scrollview`
- */ 
+ */
 declare module 'rn-faded-scrollview'
 
-/** a hack declaring empty type module to avoid type warning 
+/** a hack declaring empty type module to avoid type warning
  *  regarding `react-native-speedometer`
- */ 
+ */
 declare module 'react-native-speedometer'
 
-/** a hack declaring empty type module to avoid type warning 
+/** a hack declaring empty type module to avoid type warning
  *  regarding `Base64`
- */ 
+ */
 declare module 'Base64'
 
 type PhoneNum = string
 
+/* HTTP Response schemas */
+
+type ErrorResponse = { message: string } | { msg: string }
+type LoginOkResponse = { jwt: string }
+type RegisterOkResponse = { db_user_id: string }
+type OtpOkResponse = { otp_code: string }
+type ScoreOkResponse = { score: string }
+
+type RegisterForm = {
+  full_name: string
+  phone: string
+  email: string
+  password: string
+  license_key: string
+  role: string
+}
 type SearchResult = {
   phone: PhoneNum
   score?: number
+}
+type ProfileOkResponse = {
+  phone: string
+  full_name: string
+  bank_id: string
+  email: string
+  search_history: Array<PhoneNum>
 }
--- src/machines/API.ts
+++ src/machines/API.ts
@@ -6,21 +6,218 @@ const api = create({
   baseURL: BASE_URI,
 })
 
-/** to authenticate and get back JWT access token */
-const asyncLogin = function (phoneNum: string, rawPassword: string) {
+/** Asynchronously request to login (public route)
+ * @param phoneNum: user's input phone number as string
+ * @param rawPassword: user's input password
+ * @param onSuccess: a function takes returned access token and persist it in AsyncStorage
+ * @param onProblem: a function takes retunrned error message and display it
+ */
+const asyncLogin = (phoneNum: string, rawPassword: string) => (
+  onSuccess: (receivedToken: string) => void,
+  onProblem: (errorMessage: string) => void,
+) => {
   const decodedCreds = b64_encode(`${phoneNum}:${rawPassword}`)
-  console.log(`Decoded string: ${decodedCreds}`)
-  return api.get(
-    '/auth/login',
-    {},
-    {
-      headers: {
-        'Authorization': `Basic ${decodedCreds}`
-      }
-    },
-  )
 
-  /* Resolved object: { config: {}, data: {}, problem: string, status: number } */
+  return api
+    .get<LoginOkResponse, ErrorResponse>(
+      '/auth/login',
+      {},
+      {
+        headers: {
+          Authorization: `Basic ${decodedCreds}`,
+        },
+      },
+    )
+    .then((res) => {
+      if (!res.problem) {
+        // implement `onSuccess` callback on the received JWT string
+        onSuccess(res.data?.jwt as string)
+      } else {
+        // implement `onProblem` callback on error message
+        onProblem(Object.values(res.data as ErrorResponse)[0])
+      }
+    })
+    .catch((err) => {
+      onProblem(err.toString())
+    })
 }
 
-export { asyncLogin }
+/** Asynchronously request to logout (protected route)
+ * @param userToken: this user's session access token stored in AsyncStorage
+ * @param onSuccess: a function implement side effects (i.e. navigation stuffs) in case of success
+ * @param onProblem: <optional> a function takes returned error message and display it
+ */
+const asyncLogout = (userToken: string) => (
+  onSuccess: () => void,
+  onProblem?: (errorMessage: string) => void,
+) => {
+  return (
+    api
+      // for this route, success response has the same schema as error response
+      .post<ErrorResponse>(
+        '/auth/logout',
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
+        },
+      )
+      .then((res) => {
+        if (!res.problem) {
+          onSuccess()
+        } else {
+          // implement `onProblem` callback on error message only if `onProblem` is defined
+          onProblem && onProblem(Object.values(res.data as ErrorResponse)[0])
+        }
+      })
+      .catch((err) => {
+        if (onProblem) {
+          onProblem(err.toString())
+        } else {
+          console.error(err.stack)
+        }
+      })
+  )
+}
+
+/** Asynchronously request to register new user (public route)
+ * @param inputForm: object holds to-be-registered user's information
+ * @param onSuccess: a function implement side effects (i.e. navigation stuffs) in case of success
+ * @param onProblem: <optional> a function takes returned error message and display it
+ */
+const asyncRegister = (inputForm: RegisterForm) => (
+  onSuccess: () => void,
+  onProblem?: (errorMessage: string) => void,
+) => {
+  return api
+    .post<RegisterOkResponse, ErrorResponse>('/auth/register', inputForm)
+    .then((res) => {
+      if (!res.problem) {
+        onSuccess()
+      } else {
+        // implement `onProblem` callback on error message only if `onProblem` is defined
+        onProblem && onProblem(Object.values(res.data as ErrorResponse)[0])
+      }
+    })
+    .catch((err) => {
+      if (onProblem) {
+        onProblem(err.toString())
+      } else {
+        console.error(err.stack)
+      }
+    })
+}
+
+/** Asynchronously request an OTP code (protected route)
+ * @param userToken: this user's session access token stored in AsyncStorage
+ * @param onSuccess: a function implement side effects (i.e. navigation stuffs) in case of success
+ * @param onProblem: a function takes returned error message and display it
+ * @param customerPhone: customer's phone number
+ */
+const asyncGetOtp = (userToken: string) => (
+  onSuccess: (receivedOtpCode: string) => void,
+  onProblem: (errorMessage: string) => void,
+) => (customerPhone: string) => {
+  return api
+    .get<OtpOkResponse, ErrorResponse>(
+      '/otp',
+      { phone: customerPhone },
+      {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      },
+    )
+    .then((res) => {
+      if (!res.problem) {
+        // implement `onSuccess` callback with the received OTP code
+        onSuccess(res.data?.otp_code as string)
+      } else {
+        // implement `onProblem` callback with the received error message
+        onProblem(Object.values(res.data as ErrorResponse)[0])
+      }
+    })
+    .catch((err) => {
+      onProblem(err.toString())
+      console.error(err.stack)
+    })
+}
+
+/** Asynchronously request customer's credit score (protected route)
+ * @param userToken: this user's session access token stored in AsyncStorage
+ * @param onSuccess: a function implement side effects
+ *                   (i.e. validate, navigation stuffs, etc.) in case of success
+ * @param onProblem: a function takes returned error message and display it
+ * @param customerPhone: customer's phone number
+ */
+const asyncGetCrescore = (userToken: string) => (
+  onSuccess: (receivedScore: string) => void,
+  onProblem: (errorMessage: string) => void,
+) => (customerPhone: string) => {
+  return api
+    .get<ScoreOkResponse, ErrorResponse>(
+      '/crescore',
+      { phone: customerPhone },
+      {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      },
+    )
+    .then((res) => {
+      if (!res.problem) {
+        // implement `onSuccess` callback with the received credit score string
+        onSuccess(res.data?.score as string)
+      } else {
+        // implement `onProblem` callback with the received error message
+        onProblem(Object.values(res.data as ErrorResponse)[0])
+      }
+    })
+    .catch((err) => {
+      onProblem(err.toString())
+      console.error(err.stack)
+    })
+}
+
+/** Asynchronously request current user's profile (protected route)
+ * @param userToken: this user's session access token stored in AsyncStorage
+ * @param onSuccess: a function implement side effects
+ *                   (i.e. persist data, navigation stuffs, etc.) in case of success
+ * @param onProblem: a function takes returned error message and display it
+ */
+const asyncGetUserProfile = (userToken: string) => (
+  onSuccess: (receivedProfile: ProfileOkResponse) => void,
+  onProblem: (errorMessage: string) => void,
+) => {
+  return api
+    .get<ProfileOkResponse, ErrorResponse>(
+      '/profile',
+      {},
+      // server will decode user token to get user's phone number,
+      // which is used as login id
+      { headers: { Authorization: `Bearer ${userToken}` } },
+    )
+    .then((res) => {
+      if (!res.problem) {
+        // implement `onSuccess` callback with the received profile object
+        onSuccess(res.data as ProfileOkResponse)
+      } else {
+        // implement `onProblem` callback with the received error message
+        onProblem(Object.values(res.data as ErrorResponse)[0])
+      }
+    })
+    .catch((err) => {
+      onProblem(err.toString())
+      console.error(err.stack)
+    })
+}
+
+export {
+  asyncLogin,
+  asyncLogout,
+  asyncRegister,
+  asyncGetOtp,
+  asyncGetCrescore,
+  asyncGetUserProfile,
+}
