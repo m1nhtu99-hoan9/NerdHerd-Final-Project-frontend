6b425a15b931715e3fe75f84ffe19b0bfeffd707
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2020-09-15 11:26:01 +0700

    fix: server response 500

--- src/machines/AppMachine.ts
+++ src/machines/AppMachine.ts
@@ -5,8 +5,16 @@ import {
   AppMachineStateSchema,
 } from '../@types/machines'
 import { resetContext, persistRejectedMessage } from './appActions'
-import { isSuccessResp, isNotSuccessResp } from './appGuards'
-import { asyncLogout, asyncLogin } from './API'
+import { isSuccessResp, isNotSuccessResp, isTokenEmpty } from './appGuards'
+
+import {
+  asyncLogout,
+  asyncLogin,
+  asyncGetCrescore,
+  asyncGetUserProfile,
+  asyncGetOtp,
+  asyncRegister,
+} from './API'
 
 const AppMachine = Machine<
   AppMachineContext,
@@ -39,14 +47,16 @@ const AppMachine = Machine<
     AUTHENTICATING: {
       /* invoke login promise here:
          in case of success, transit to "LOGGED_IN",
-         else, transit back to "UNAUTHORISED".
+         else, transit to "FAILURE" 
+         ("FAILURE" implies that `context.lastResponse.lastErrorMessage` is not empty,
+          while "UNAUTHORISED" implies that that context field is empty).
       */
 
       // @ts-ignore
       invoke: {
         id: 'login_promise',
-        // @ts-ignore <HORRIBLE ERROR MAY HAPPEND HERE>
-        src: (_, event) => asyncLogin(event.phoneNum, event.password),
+        src: (_, event) =>
+          asyncLogin(event.phoneNum as string, event.password as string),
         onDone: [
           {
             /* in case of 200's response */
@@ -73,7 +83,7 @@ const AppMachine = Machine<
                   statusCode,
                   lastErrorMessage:
                     statusCode >= 500
-                      ? JSON.stringify(respData)
+                      ? 'Internal server error'
                       : (Object.values(respData)[0] as string),
                 }
               },
@@ -97,42 +107,102 @@ const AppMachine = Machine<
         Logout: { target: 'UNAUTHORISED' },
       },
     },
-    INVOKING_PROFILE_PROMISE: {},
-    PROFILE_UPDATED: {},
-    PREPARING_HOME_SCREEN: {
-      /* invoke multiple crescore promises to query credit score of each 
-         phone number in search history */
-    },
-    READY: {
-      /* HomeScreen is ready to display */
-      on: {
-        Logout: { target: 'UNAUTHORISED' },
-      },
-    },
-    INVOKING_OTP_PROMISE: {},
-    OTP_UPDATED: {},
-    INVOKING_CRESCORE_PROMISE: {},
-    SEARCH_HISTORY_UPDATED: {},
-    LOGGING_OUT: {
-      /* @ts-ignore; request the server to revoke current user token */
+    INVOKING_PROFILE_PROMISE: {
+      /* invoke login promise here:
+         in case of success, transit to "PROFILE_UPDATED",
+         else, transit to "FAILURE".
+      */
+
+      // @ts-ignore
       invoke: {
-        id: 'logout_promise',
-        src: (ctx) => asyncLogout(ctx.token as string),
-        onDone: {
-          target: 'UNAUTHORISED',
-          actions: resetContext,
-        },
+        id: 'profile_promise',
+        src: (ctx, event) => asyncGetUserProfile(ctx.token as string),
+        onDone: [
+          {
+            /* in case of 200's response */
+            cond: isSuccessResp,
+            target: 'LOGGED_IN',
+            actions: assign({
+              // `event.data` returns an object complying to interface `ApiOkResponse`
+              token: (_, event) => event.data.data.jwt,
+              lastResponse: (_, event) => {
+                return { statusCode: event.data.status, lastErrorMessage: '' }
+              },
+            }),
+          },
+          {
+            /* other types of response */
+            cond: isNotSuccessResp,
+            target: 'FAILURE',
+            actions: assign({
+              lastResponse: (_, event) => {
+                const statusCode = event.data.status
+                const respData = event.data.data
+
+                return {
+                  statusCode,
+                  lastErrorMessage:
+                    statusCode >= 500
+                      ? 'Internal server error'
+                      : (Object.values(respData)[0] as string),
+                }
+              },
+            }),
+          },
+        ],
         onError: {
           target: 'FAILURE',
-          actions: persistRejectedMessage,
+          actions: assign<AppMachineContext, AppMachineEvent>({
+            // for rejected promises, `event.data` returns error message
+            lastResponse: (ctx, event) => {
+              return { ...ctx.lastResponse, lastErrorMessage: event.data }
+            },
+          }),
         },
       },
     },
-    FAILURE: {
-      on: {
-        Logout: { target: 'UNAUTHORISED' },
-        Login: { target: 'AUTHENTICATING' },
+  },
+  PROFILE_UPDATED: {},
+  PREPARING_HOME_SCREEN: {
+    /* invoke multiple crescore promises to query credit score of each 
+         phone number in search history */
+  },
+  READY: {
+    /* HomeScreen is ready to display */
+    on: {
+      Logout: { target: 'UNAUTHORISED' },
+    },
+  },
+  INVOKING_OTP_PROMISE: {},
+  OTP_UPDATED: {},
+  INVOKING_CRESCORE_PROMISE: {},
+  SEARCH_HISTORY_UPDATED: {},
+  LOGGING_OUT: {
+    /* request the server to revoke current user token */
+    invoke: {
+      id: 'logout_promise',
+      src: (ctx: AppMachineContext) => asyncLogout(ctx.token as string),
+      onDone: {
+        target: 'UNAUTHORISED',
+        actions: resetContext,
       },
+      onError: {
+        target: 'FAILURE',
+        actions: persistRejectedMessage,
+      },
+    },
+  },
+  FAILURE: {
+    always: [
+      {
+        cond: isTokenEmpty,
+        target: 'UNAUTHORISED',
+      },
+    ],
+    on: {
+      Logout: { target: 'UNAUTHORISED' },
+      // a bit excessive but it's ok, ya know へ‿(ツ)‿ㄏ
+      Login: { target: 'AUTHENTICATING' },
     },
   },
 })
